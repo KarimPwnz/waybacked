b23301a4def952a27309b90fb7a4871a39814d39
Karim Rahal <karim@karimrahal.com>
2021-12-12 15:27:23 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="waybacked",
-    author="Karim Rahal",
-    author_email="karim@karimrahal.com",
     version="0.1",
     license="MIT",
+    author="karimpwnz",
+    author_email="karim@karimrahal.com",
+    description="Get URLs from the Wayback Machine.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/karimpwnz/waybacked",
+    project_urls={
+        "Bug Tracker": "https://github.com/karimpwnz/waybacked/issues", },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Security",
+    ],
     packages=["waybacked"],
     entry_points={"console_scripts": ["waybacked = waybacked.__main__:main"]},
     python_requires=">=3.6",
